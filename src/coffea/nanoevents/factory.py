6700d16d83f0904a36bd6b1863e00a145d23537f
Giedrius <giedrius.mail.11@gmail.com>
2024-08-06 13:34:25 +0200

    Testing changes of coffea

--- src/coffea/nanoevents/factory.py
+++ src/coffea/nanoevents/factory.py
@@ -387,7 +387,6 @@ class NanoEventsFactory:
         base_form = mapping._extract_base_form(
             tree, iteritems_options=iteritems_options
         )
-        print("Base form debug: ", base_form)
 
         return cls._from_mapping(
             mapping,
